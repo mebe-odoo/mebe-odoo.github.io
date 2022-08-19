7e8cdb6fa56ee4c46cb86ce4d5eac57e975dc60d
Mehdi Bendali Hacine <bendali.hacine.m@gmail.com>
2022-08-19 16:20:50 +0400

    Create new Vuepress app

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: "TCO Training",
+    description: "Static website for sharing technical exercises and tutorials",
+    themeConfig: {
+        sidebarDepth: 2,
+        sidebar: [
+            "/",
+            {
+                title: "Exercises",
+                collapsable: false,
+                path: "/tco/",
+                children: [
+                    {
+                        title: "Realty Management - Part 1",
+                        path: "/tco/exercises/realty_management.html",
+                    },
+                    {
+                        title: "Realty Management - Part 2",
+                        path: "/tco/exercises/realty_management_2.html",
+                    },
+                ],
+            },
+            {
+                title: "Solutions",
+                collapsable: false,
+                path: "/tco/solutions/",
+                children: [],
+            },
+        ],
+    },
+};
